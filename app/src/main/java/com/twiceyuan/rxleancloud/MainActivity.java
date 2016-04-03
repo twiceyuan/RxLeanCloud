133dfd46214339d95b407058037a267ab7e8c45b
twiceyuan <twiceyuan@gmail.com>
2016-04-04 01:18:00 +0800

    update example

--- app/src/main/java/com/twiceyuan/rxleancloud/MainActivity.java
+++ app/src/main/java/com/twiceyuan/rxleancloud/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.avos.avoscloud.AVACL;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.AVRole;
 import com.avos.avoscloud.SaveCallback;
 
 import cn.leancloud.rx.LeanCallbacks;
@@ -25,12 +27,30 @@ public class MainActivity extends AppCompatActivity {
     private void simple() {
         // 测试 SDK 是否正常工作的代码
         AVObject testObject = new AVObject("TestObject");
-        testObject.put("words","Hello World!");
+        testObject.put("words", "Hello World!");
+        // 保存对象
         testObject.saveInBackground(new SaveCallback() {
             @Override
             public void done(AVException e) {
-                if(e == null){
-                    Log.d("saved","simple success!");
+                if (e == null) {
+                    String objectId = testObject.getObjectId();
+                    // 创建角色
+                    AVRole role = new AVRole(objectId);
+                    role.saveInBackground(new SaveCallback() {
+                        @Override public void done(AVException e) {
+                            // 设置角色权限
+                            AVACL avacl = new AVACL();
+                            avacl.setRoleReadAccess(role, true);
+                            testObject.saveInBackground(new SaveCallback() {
+                                @Override public void done(AVException e) {
+                                    if (e == null) {
+                                        // 保存成功
+                                        Log.i("saved", "save success!");
+                                    }
+                                }
+                            });
+                        }
+                    });
                 }
             }
         });
@@ -38,8 +58,24 @@ public class MainActivity extends AppCompatActivity {
 
     private void rx() {
         AVObject testObject = new AVObject("TestObject");
-        testObject.put("words","Hello World!");
-        Observable.create(subscriber -> testObject.saveInBackground(LeanCallbacks.saveRx(subscriber)))
-            .subscribe(o -> Log.d("saved","rx success!"));
+        testObject.put("words", "Hello World!");
+        Observable.<AVObject>create(subscriber -> {
+            // 保存对象
+            testObject.setFetchWhenSave(true);
+            testObject.saveInBackground(LeanCallbacks.saveRx(subscriber));
+        }).<AVRole>flatMap(object -> Observable.create(subscriber -> {
+            // 创建角色
+            AVRole role = new AVRole(object.getObjectId());
+            role.setFetchWhenSave(true);
+            role.saveInBackground(LeanCallbacks.saveRx(subscriber));
+        })).flatMap(role -> Observable.create(subscriber -> {
+            // 设置角色权限
+            testObject.setACL(new AVACL());
+            testObject.getACL().setRoleReadAccess(role, true);
+            testObject.saveInBackground(LeanCallbacks.saveRx(subscriber));
+        })).subscribe(success -> {
+            // 保存成功
+            Log.d("saved", "rx save success");
+        });
     }
 }
